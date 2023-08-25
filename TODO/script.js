29900b597b9b8a8978b295803e556f904226be62
Husnain-Ashfaq39 <i222536@nu.edu.pk>
2023-08-25 23:57:21 +0500

    Todo Remove Functionality added

--- TODO/script.js
+++ TODO/script.js
@@ -1,25 +1,43 @@
 let inputField = document.querySelector('.inputField');
 let addButton = document.querySelector('.button');
 
+let parent = document.querySelector('.container');
 
-addButton.addEventListener('click', function(e)
-{
+
+addButton.addEventListener('click', function (e) {
     e.preventDefault();
-    let taskString=inputField.value;
-    let div=document.createElement('div');
+    let taskString = inputField.value;
+    let div = document.createElement('div');
     div.classList.add('task');
     console.log(div);
-    let tick=document.createElement('input');
-    tick.type='checkbox';
-    let task=document.createElement('label');
-    task.innerHTML=taskString;
-    console.log(tick);
-    console.log(task);
+    let tick = document.createElement('input');
+    tick.type = 'checkbox';
+    let task = document.createElement('label');
+    task.innerHTML = taskString;
+    let img = document.createElement('img');
+    img.src = 'close.png';
+    img.id = 'close';
+
     div.appendChild(tick);
     div.appendChild(task);
-    let parent=document.querySelector('.container');
+    div.appendChild(img);
+
+
     parent.appendChild(div);
-
-
+    deletion();
 
 })
+
+
+function deletion() {
+    let delButton = document.querySelectorAll('#close');
+    delButton.forEach(function (delButn) {
+        delButn.addEventListener('click', function () {
+            let taskDiv = delButn.parentElement;
+            console.log('Hello');
+            taskDiv.remove(); // Remove the task div when close button is clicked
+        });
+    });
+
+}
+deletion();
