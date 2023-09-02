4938e8bd157eb0c8321c6846a3840bd9371349c4
Husnain-Ashfaq39 <i222536@nu.edu.pk>
2023-09-02 11:02:31 +0500

    Generate Random Password Almost completed

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -6,46 +6,70 @@ let lowerCase = document.getElementById('include-lowercase');
 let number = document.getElementById('include-numbers');
 let symbol_box = document.getElementById('include-symbols');
 
-let zero = document.getElementsByClassName('zero');
+
 let weak = document.getElementsByClassName('weak');
 let normal = document.getElementsByClassName('normal');
 let strong = document.getElementsByClassName('strong');
 
+
+
 let copy_button = document.querySelector('#copy-button');
 const passwordField = document.getElementById('password-field');
+const generateButton = document.getElementById('generate-button');
+console.log(generateButton);
 
 let password = "Password";
 let password_length = 10;
 let checkCount = 1;
 const Symbols = '~!@#$%^&*()_+}{:"?></.,;][]';
 
-HandleSlider();
+lengthBar.addEventListener('input', HandleSlider);
 function HandleSlider() {
 
-    lengthBar.value = password_length
-    length_Status.textContent = password_length;
+    password_length = lengthBar.value;
+    length_Status.textContent = lengthBar.value;
+
 }
 
 let hasUpperCase = false;
 let hasLowerCase = false;
 let hasNumbers = false;
 let hasSymbols = false;
-if (upperCase.checked) hasUpperCase = true;
-if (lowerCase.checked) hasLowerCase = true;
-if (number.checked) hasNumbers = true;
-if (symbol_box.checked) hasSymbols = true;
 
-// handleStrength();
+function tick_checker() {
+    hasUpperCase = false;
+    hasLowerCase = false;
+    hasNumbers = false;
+    hasSymbols = false;
+    if (upperCase.checked) hasUpperCase = true;
+    if (lowerCase.checked) hasLowerCase = true;
+    if (number.checked) hasNumbers = true;
+    if (symbol_box.checked) hasSymbols = true;
+}
+
+
+
 function handleStrength() {
+
+
     if (hasUpperCase && hasLowerCase && (hasNumbers || hasSymbols) && password_length >= 8) {
-        strong.style.display = "flex";
+        strength_disabler()
+        strong[0].style.display = 'flex';
     }
     else if (hasUpperCase && hasLowerCase && password_length >= 8) {
-        normal.style.display = "flex";
+        strength_disabler()
+        normal[0].style.display = "flex";
     }
     else if (hasUpperCase && hasLowerCase && password_length <= 8) {
-        weak.style.display = "flex";
+        strength_disabler()
+        weak[0].style.display = "flex";
     }
+}
+
+function strength_disabler() {
+    weak[0].style.display = "none";
+    normal[0].style.display = "none";
+    strong[0].style.display = 'none';
 }
 
 function getRandomInteger(min, max) {
@@ -79,35 +103,7 @@ function generateSymbol() {
 }
 
 
-function CopyContent() {
 
-    try {
-
-        navigator.clipboard.writeText(passwordField.value)
-        copy_button.innerHTML = 'Copied';
-        setTimeout(function () {
-            copy_button.innerHTML = `<i class="fas fa-copy active"></i>`;
-            console.log(copy_button.innerHTML);
-        }, 3000)
-    }
-    catch (e) {
-        copy_button.innerHTML = 'Failed to copy';
-        setTimeout(function () {
-            copy_button.innerHTML = `<i class="fas fa-copy active"></i>`;
-            console.log(copy_button.innerHTML);
-        }, 3000)
-    }
-    // const password = passwordField.value;
-
-    // // Use the Clipboard API to write text to the clipboard
-    // navigator.clipboard.writeText(password)
-    //     .then(() => {
-    //         console.log('Text copied to clipboard:', password);
-    //     })
-    //     .catch(err => {
-    //         console.error('Error copying text:', err);
-    //     });
-}
 copy_button.addEventListener('click', function () {
     try {
 
@@ -123,7 +119,44 @@ copy_button.addEventListener('click', function () {
         setTimeout(function () {
             copy_button.innerHTML = `<i class="fas fa-copy active"></i>`;
             console.log(copy_button.innerHTML);
-        }, 3000)}
+        }, 3000)
+    }
 });
 
 
+generateButton.addEventListener('click', generate_Password());
+function generate_Password() {
+
+
+    //Used the Concept of Closure______
+    return function () {
+        let pass = '';
+        let i = 0;
+        tick_checker();
+        while (i < password_length) {
+            if (hasNumbers) {
+                pass += generateRandomNumber().toString();
+                i++;
+            }
+
+            if (hasUpperCase) {
+                pass += generateUpperCase().toString().toUpperCase();
+                i++;
+            }
+            if (hasLowerCase) {
+                pass += generateLowerCase().toString();
+                i++;
+            }
+            if (hasSymbols) {
+                pass += generateSymbol().toString();
+                i++;
+            }
+        }
+
+        handleStrength();
+        passwordField.value = pass;
+
+    }
+
+
+}
